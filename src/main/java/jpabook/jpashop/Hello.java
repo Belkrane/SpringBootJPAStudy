553e9024ba7b17ab32b7a29390b6156f4bef745d
Belkrane <jonghagi77@gmail.com>
2023-04-20 13:41:20 +0900

    init Project

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Hello {
+    private String data;
+}

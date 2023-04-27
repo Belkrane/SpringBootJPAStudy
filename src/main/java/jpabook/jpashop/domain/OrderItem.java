86958f4497644ec57ea3bd7997b66064337187e8
Belkrane <jonghagi77@gmail.com>
2023-04-27 22:16:47 +0900

    Implement Order Entity Busines Logic

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -18,7 +18,7 @@ public class OrderItem {
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
-    private Item items;
+    private Item item;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
@@ -27,4 +27,23 @@ public class OrderItem {
     private int orderPrice; //주문가격
     private int count; //주문수량
 
+    //== 생성 메서드 ==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비지니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
